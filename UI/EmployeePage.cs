de85835099603478c50faa5c7d528a32f4cfdb3a
cinthyaJaneth <cinthya.janeth.ruvalcaba@gmail.com>
2025-07-14 14:50:16 -0600

    Add files via upload
    Adding employee and login pages

---
+++ UI/EmployeePage.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+
+
+namespace PlaywrightTestPaylocity.UI
+{
+    internal class EmployeePage
+    {
+        private readonly IPage _page;
+
+        public EmployeePage(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task AddEmployeeAsync(string firstName, string lastName, string dependants)
+        {
+            await _page.ClickAsync("//button[text()='Add Employee']");
+            await _page.FillAsync("//input[@id='firstName']", firstName);
+            await _page.FillAsync("//input[@id='lastName']", lastName);
+            await _page.FillAsync("//input[@id='dependants']", dependants);
+            await _page.ClickAsync("//button[text()='Add']");
+        }
+
+        public async Task EditEmployeeAsync(string firstName, string lastName, string dependants, string newFirstName)
+        {
+            string rowXPath = $"//table[@id='employeesTable']//tr[td[text()='{lastName}'] and td[text()='{firstName}'] and td[text()='{dependants}']]";
+            string editIconXPath = $"{rowXPath}//i[contains(@class, 'fa-edit')]";
+            await _page.ClickAsync(editIconXPath);
+            await _page.FillAsync("//input[@id='firstName']", newFirstName);
+            await _page.ClickAsync("//button[text()='Update']");
+        }
+
+        public async Task DeleteEmployeeAsync(string firstName, string lastName, string dependants)
+        {
+            string rowXPath = $"//table[@id='employeesTable']//tr[td[text()='{lastName}'] and td[text()='{firstName}'] and td[text()='{dependants}']]";
+            string deleteIconXPath = $"{rowXPath}//i[contains(@class, 'fa-times')]";
+            await _page.ClickAsync(deleteIconXPath);
+            await _page.ClickAsync("//button[text()='Delete']");
+        }
+
+    }
+}
---
+++ UI/LoginPage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Playwright;
+
+
+namespace PlaywrightTestPaylocity.UI
+{
+    internal class LoginPage
+    {
+        private readonly IPage _page;
+
+        public LoginPage(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task NavigateAsync()
+        {
+            await _page.GotoAsync("https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login");
+        }
+
+        public async Task LoginAsync(string username, string password)
+        {
+            await _page.FillAsync("//input[@name='Username']", username);
+            await _page.FillAsync("//input[@name='Password']", password);
+            await _page.ClickAsync("text='Log In'");
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        }
+    }
+}
